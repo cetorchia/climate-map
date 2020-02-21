b3923dd1203fecb52106f36f8b13be7b95cf2bdc
Carlos Torchia <cetorchia@gmail.com>
2020-02-21 03:46:00 -0500

    Ensure latitudes and longitudes completely line up in calibration

--- src/climatetransform.py
+++ src/climatetransform.py
@@ -38,6 +38,8 @@ AVERAGE_FEB_DAYS = 28.2425
 SCALE_FACTOR = 10
 ABSOLUTE_DIFFERENCE_MEASUREMENTS = 'tavg', 'tmin', 'tmax'
 OUTPUT_DTYPE = np.int16
+OUTPUT_DTYPE_MIN = np.iinfo(OUTPUT_DTYPE).min
+OUTPUT_DTYPE_MAX = np.iinfo(OUTPUT_DTYPE).max
 
 # Max latitude in OpenStreetMap, about 85.0511
 # https://en.wikipedia.org/wiki/Web_Mercator_projection#Formulas
@@ -185,8 +187,8 @@ def pack_array(data_arr, units=None):
     data_arr *= SCALE_FACTOR
 
     dtype = OUTPUT_DTYPE
-    MIN_DTYPE = np.iinfo(dtype).min
-    MAX_DTYPE = np.iinfo(dtype).max
+    MIN_DTYPE = OUTPUT_DTYPE_MIN
+    MAX_DTYPE = OUTPUT_DTYPE_MAX
 
     missing_value = data_arr.fill_value
 
@@ -454,9 +456,7 @@ def normalize_longitudes(lon_arr, data_arr):
     example, the longitudes starting at 180 will be moved to start at -180.
     Assumes axis 1 in the data array contains the longitudes.
     '''
-    # Must be strictly increasing.
-    # See https://stackoverflow.com/a/4983359
-    if not all(x<y for x, y in zip(lon_arr, lon_arr[1:])):
+    if not is_increasing(lon_arr):
         raise Exception('Longitudes are not strictly increasing')
 
     gt180_idxs = np.where(lon_arr >= 180)[0]
@@ -479,17 +479,22 @@ def normalize_latitudes(lat_arr, data_arr):
 
     Assumes axis 0 in the data array contains the latitudes.
     '''
-    # Must be strictly increasing or decreasing.
-    # Ensure strictly decreasing.
-    # See https://stackoverflow.com/a/4983359
-    if all(x<y for x, y in zip(lat_arr, lat_arr[1:])):
+    if is_increasing(lat_arr):
         new_lat_arr, new_data_arr = lat_arr[::-1], data_arr[::-1]
-    elif all(x>y for x, y in zip(lat_arr, lat_arr[1:])):
+    elif is_decreasing(lat_arr):
         new_lat_arr, new_data_arr = lat_arr, data_arr
     else:
         raise Exception('Latitudes are not strictly increasing or decreasing')
 
     return new_lat_arr, new_data_arr
+
+def is_increasing(arr):
+    # See https://stackoverflow.com/a/4983359
+    return all(x<y for x, y in zip(arr, arr[1:]))
+
+def is_decreasing(arr):
+    # See https://stackoverflow.com/a/4983359
+    return all(x>y for x, y in zip(arr, arr[1:]))
 
 def data_to_standard_units(units, data_arr, month):
     '''
@@ -901,6 +906,182 @@ def save_db_data(
 
         climatedb.commit()
 
+def axis_limit_arrays(axis_arr, axis_delta):
+    '''
+    Returns left and right axis limit arrays. Each element provides
+    the limit that points in the range of the coordinate must fall right
+    and left of which respectively.
+    '''
+    left_axis_limit = axis_arr[0] - axis_delta / 2
+    right_axis_limit = axis_arr[-1] + axis_delta / 2
+
+    left_axis_limit_arr = np.empty(axis_arr.size)
+    left_axis_limit_arr[0] = left_axis_limit
+    left_axis_limit_arr[1:] = (axis_arr[:-1] + axis_arr[1:]) / 2
+
+    right_axis_limit_arr = np.empty(axis_arr.size)
+    right_axis_limit_arr[:-1] = (axis_arr[:-1] + axis_arr[1:]) / 2
+    right_axis_limit_arr[-1] = right_axis_limit
+
+    if axis_delta > 0:
+        return left_axis_limit_arr, right_axis_limit_arr
+    else:
+        return right_axis_limit_arr, left_axis_limit_arr
+
+def downscale_axis_arr(baseline_axis_arr, axis_arr, left_axis_limit_arr, right_axis_limit_arr):
+    '''
+    Downscales the lower resolution axis array to the higher resolution baseline axis array.
+    '''
+    axis_repeats = np.array([
+        len(np.where(
+            (baseline_axis_arr >= left_axis_limit) &
+            (baseline_axis_arr < right_axis_limit)
+        )[0])
+        for left_axis_limit, right_axis_limit
+        in zip(left_axis_limit_arr, right_axis_limit_arr)
+    ])
+
+    if left_axis_limit_arr[0] < left_axis_limit_arr[-1]:
+        mask_left = len(np.where((baseline_axis_arr < left_axis_limit_arr[0]))[0])
+        mask_right = len(np.where((baseline_axis_arr >= right_axis_limit_arr[-1]))[0])
+    else:
+        mask_left = len(np.where((baseline_axis_arr >= right_axis_limit_arr[0]))[0])
+        mask_right = len(np.where((baseline_axis_arr < left_axis_limit_arr[-1]))[0])
+
+    if axis_arr[-1] < axis_arr[0]:
+        mask_left, mask_right = mask_right, mask_left
+
+    if baseline_axis_arr.size != mask_left + axis_repeats.sum() + mask_right:
+        raise Exception('Expected baseline axis to have size mask_left + axis_repeats + mask_right = %d' % (
+                        mask_left + axis_repeats.sum() + mask_right))
+
+    downscaled_axis_arr = np.ma.zeros(baseline_axis_arr.size)
+    downscaled_axis_arr.mask = np.repeat(False, downscaled_axis_arr.size)
+    downscaled_axis_arr.mask[:mask_left] = True
+    downscaled_axis_arr.mask[-mask_right:] = True
+    downscaled_axis_arr[mask_left:-mask_right] = np.repeat(axis_arr, axis_repeats)
+
+    num_downscaled = np.nonzero(~downscaled_axis_arr.mask)[0].size
+
+    if num_downscaled != axis_repeats.sum():
+        raise Exception('Expected number %d of non-masked downscaled axis elements to be %d' % (
+            num_downscaled, axis_repeats.sum()))
+
+    return mask_left, mask_right, downscaled_axis_arr, axis_repeats
+
+def check_downscaled_axis_arr(
+        baseline_axis_arr,
+        downscaled_axis_arr,
+        left_axis_limit_arr,
+        right_axis_limit_arr,
+        axis_repeats
+):
+    '''
+    Checks that the downscaled axis array corresponds to the each coordinate
+    in the baseline. Throws an exception if not.
+    '''
+    downscaled_left_axis_limit_arr = np.ma.empty_like(downscaled_axis_arr)
+    downscaled_left_axis_limit_arr.mask = downscaled_axis_arr.mask.copy()
+    downscaled_left_axis_limit_arr[~downscaled_axis_arr.mask] = np.repeat(left_axis_limit_arr, axis_repeats)
+
+    downscaled_right_axis_limit_arr = np.ma.empty_like(downscaled_axis_arr)
+    downscaled_right_axis_limit_arr.mask = downscaled_axis_arr.mask.copy()
+    downscaled_right_axis_limit_arr[~downscaled_axis_arr.mask] = np.repeat(right_axis_limit_arr, axis_repeats)
+
+    invalid_baseline_coordinates = (
+        (baseline_axis_arr < downscaled_left_axis_limit_arr) |
+        (baseline_axis_arr >= downscaled_right_axis_limit_arr)
+    )
+
+    if np.any(invalid_baseline_coordinates):
+        raise Exception('Baseline coordinates %a out of bounds for downscaled %a' % (
+            baseline_axis_arr[invalid_baseline_coordinates],
+            downscaled_axis_arr[invalid_baseline_coordinates]
+        ))
+
+    invalid_downscaled_coordinates = (
+        (downscaled_axis_arr < downscaled_left_axis_limit_arr) |
+        (downscaled_axis_arr >= downscaled_right_axis_limit_arr)
+    )
+
+    if np.any(invalid_downscaled_coordinates):
+        raise Exception('Downscaled coordinates %a out of bounds for %a,%a' % (
+            downscaled_axis_arr[invalid_downscaled_coordinates],
+            downscaled_left_axis_limit_arr[invalid_downscaled_coordinates],
+            downscaled_right_axis_limit_arr[invalid_downscaled_coordinates]
+        ))
+
+def downscale_array(baseline_lat_arr, baseline_lon_arr, lat_arr, lat_delta, lon_arr, lon_delta, data_arr):
+    '''
+    Downscales the lower-resolution data to the specified higher-resolution baseline data.
+    '''
+    baseline_lat_increasing = is_increasing(baseline_lat_arr)
+    baseline_lat_decreasing = is_decreasing(baseline_lat_arr)
+
+    if not baseline_lat_decreasing and not baseline_lat_increasing:
+        raise Exception('Expected baseline latitudes to be increasing or decreasing')
+
+    baseline_lon_increasing = is_increasing(baseline_lon_arr)
+    baseline_lon_decreasing = is_decreasing(baseline_lon_arr)
+
+    if not baseline_lon_decreasing and not baseline_lon_increasing:
+        raise Exception('Expected baseline longitudes to be increasing or decreasing')
+
+    lat_increasing = is_increasing(lat_arr)
+    lat_decreasing = is_decreasing(lat_arr)
+
+    if not lat_decreasing and not lat_increasing:
+        raise Exception('Expected latitudes to be increasing or decreasing')
+
+    lon_increasing = is_increasing(lon_arr)
+    lon_decreasing = is_decreasing(lon_arr)
+
+    if not lon_decreasing and not lon_increasing:
+        raise Exception('Expected longitudes to be increasing or decreasing')
+
+    if (lat_increasing and baseline_lat_decreasing) \
+    or (lat_decreasing and baseline_lat_increasing):
+        lat_arr = lat_arr[::-1]
+        lat_delta = -lat_delta
+
+    if (lon_increasing and baseline_lon_decreasing) \
+    or (lon_decreasing and baseline_lon_increasing):
+        lon_arr = lon_arr[::-1]
+        lon_delta = -lon_delta
+
+    left_lat_limit_arr, right_lat_limit_arr = axis_limit_arrays(lat_arr, lat_delta)
+    lat_mask_left, lat_mask_right, downscaled_lat_arr, lat_repeats = downscale_axis_arr(baseline_lat_arr, lat_arr,
+                                                                                        left_lat_limit_arr,
+                                                                                        right_lat_limit_arr)
+
+    left_lon_limit_arr, right_lon_limit_arr = axis_limit_arrays(lon_arr, lon_delta)
+    lon_mask_left, lon_mask_right, downscaled_lon_arr, lon_repeats = downscale_axis_arr(baseline_lon_arr, lon_arr,
+                                                                                        left_lon_limit_arr,
+                                                                                        right_lon_limit_arr)
+
+    check_downscaled_axis_arr(baseline_lat_arr, downscaled_lat_arr,
+                              left_lat_limit_arr, right_lat_limit_arr, lat_repeats)
+    check_downscaled_axis_arr(baseline_lon_arr, downscaled_lon_arr,
+                              left_lon_limit_arr, right_lon_limit_arr, lon_repeats)
+
+    downscaled_data_arr = np.ma.empty((downscaled_lat_arr.size, downscaled_lon_arr.size))
+    downscaled_data_arr.mask = np.repeat(False, downscaled_data_arr.size).reshape(downscaled_data_arr.shape)
+    downscaled_data_arr.set_fill_value(
+        data_arr.fill_value if isinstance(data_arr, np.ma.masked_array) else OUTPUT_DTYPE_MIN
+    )
+
+    downscaled_data_arr.mask[downscaled_lat_arr.mask, :] = True
+    downscaled_data_arr.base[downscaled_lat_arr.mask, :] = downscaled_data_arr.fill_value
+    downscaled_data_arr.mask[:, downscaled_lon_arr.mask] = True
+    downscaled_data_arr.base[:, downscaled_lon_arr.mask] = downscaled_data_arr.fill_value
+
+    downscaled_data_subarr = np.repeat(data_arr, lat_repeats, axis=0)
+    downscaled_data_subarr = np.repeat(downscaled_data_subarr, lon_repeats, axis=1)
+
+    downscaled_data_arr[lat_mask_left:-lat_mask_right, lon_mask_left:-lon_mask_right] = downscaled_data_subarr
+
+    return downscaled_data_arr
+
 def calibrate(
         baseline_data_source_code,
         historical_data_source_code,
@@ -963,14 +1144,28 @@ def calibrate(
     if projection_data.shape != historical_data.shape:
         raise Exception('Expected historical data to have the same shape as projection')
 
+    if np.any(projection_lat != historical_lat):
+        raise Exception('Expected historical latitudes to be the same as projection latitudes')
+
+    if np.any(projection_lon != historical_lon):
+        raise Exception('Expected historical longitudes to be the same as projection longitudes')
+
     if measurement in ABSOLUTE_DIFFERENCE_MEASUREMENTS:
         print('Using absolute difference')
-        differences = np.float(projection_data - historical_data)
+        differences = np.float64(projection_data - historical_data)
     else:
         print('Using relative difference')
-        differences = projection_data / historical_data
-        extreme_differences = (differences >= 5)
+        differences = projection_data.copy()
+
+        historically_nonzero = (historical_data != 0)
+        differences[historically_nonzero] = \
+            projection_data[historically_nonzero] / historical_data[historically_nonzero]
+
+        extreme_differences = (differences >= 5) & historically_nonzero
         differences[extreme_differences] = projection_data[extreme_differences] - historical_data[extreme_differences]
+
+        if np.any(np.isnan(differences)):
+            raise Exception('Unexpected NaN value(s) after dividing')
 
     print('Against baseline dataset %s-%d-%d-%s-%s' % (
         baseline_data_source_code,
@@ -989,9 +1184,15 @@ def calibrate(
     )
     baseline_lat, baseline_lon, baseline_data = climatedb.fetch_normals_from_dataset(baseline_dataset, month)
 
-    baseline_height, baseline_width = baseline_data.shape
-
-    downscaled_differences = cv2.resize(differences, (baseline_width, baseline_height), interpolation=cv2.INTER_NEAREST)
+    downscaled_differences = downscale_array(
+        baseline_lat,
+        baseline_lon,
+        projection_lat,
+        projection_dataset['lat_delta'],
+        projection_lon,
+        projection_dataset['lon_delta'],
+        differences
+    )
 
     if downscaled_differences.shape != baseline_data.shape:
         raise Exception('Expected downscaled differences to have the same shape as baseline data')
@@ -1003,7 +1204,7 @@ def calibrate(
         extreme_differences = (downscaled_differences >= 5)
         calibrated_data[extreme_differences] = baseline_data[extreme_differences] + downscaled_differences[extreme_differences]
 
-    if np.any(calibrated_data > np.iinfo(OUTPUT_DTYPE).max):
+    if np.any(calibrated_data > OUTPUT_DTYPE_MAX):
         raise Exception('Calibrated data is out of bounds for %s' % OUTPUT_DTYPE)
     else:
         calibrated_data = calibrated_data.astype(OUTPUT_DTYPE)

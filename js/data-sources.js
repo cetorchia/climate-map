8effb797ff60211b93dad6361dfd73ec96f45970
Carlos Torchia <cetorchia@gmail.com>
2020-07-30 05:50:24 -0400

    Fix SSP scenario button placement

--- js/data-sources.js
+++ js/data-sources.js
@@ -159,9 +159,9 @@ function showSSPButton()
     const about_button = document.getElementById('about-button');
     const legend_div = document.getElementById('legend');
 
-    units_button.style.top = '16.8rem';
-    about_button.style.top = '18.9rem';
-    legend_div.style.top = '21rem';
+    units_button.style.top = '20.6rem';
+    about_button.style.top = '23.3rem';
+    legend_div.style.top = '26rem';
 }
 
 /**
@@ -177,9 +177,9 @@ function hideSSPButton()
     const about_button = document.getElementById('about-button');
     const legend_div = document.getElementById('legend');
 
-    units_button.style.top = '14.6rem';
-    about_button.style.top = '16.7rem';
-    legend_div.style.top = '18.9rem';
+    units_button.style.top = '18.1rem';
+    about_button.style.top = '20.6rem';
+    legend_div.style.top = '23.3rem';
 }
 
 /**
